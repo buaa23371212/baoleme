7882f9843008a1f6908a13c523ad35c96b89300c
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-20 17:06:50 +0800

    commit

---
+++ src/main/java/org/demo/baoleme/pojo/Product.java
@@ -0,0 +1,40 @@
+package org.demo.baoleme.pojo;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@TableName("product")
+public class Product {
+
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    private Long storeId;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private String category;
+
+    private Integer stock;
+
+    private BigDecimal rating;
+
+    /**
+     * 状态（1-开启，0-关闭）
+     */
+    private Integer status;
+
+    private LocalDateTime createdAt;
+
+    private String image;
+}
