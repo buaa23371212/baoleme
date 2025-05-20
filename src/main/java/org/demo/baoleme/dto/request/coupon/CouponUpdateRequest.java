59682204ccd147a95fe3549d239c9ddfa10511cd
胡淞程 <15522917+buaa23371212@user.noreply.gitee.com>
2025-05-20 15:07:58 +0800

    commit and 1 problem

--- src/main/java/org/demo/baoleme/dto/request/coupon/CouponUpdateRequest.java
+++ src/main/java/org/demo/baoleme/dto/request/coupon/CouponUpdateRequest.java
@@ -7,7 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 @Data
-public class CouponUpdateRequset {
+public class CouponUpdateRequest {
     private Long id;
     private String desc;
     private LocalDateTime startAt;
